280485bea4d74acbdd989acc0ba1314c05629c6d
Sranav <39919607+Sranav@users.noreply.github.com>
2020-07-08 13:06:31 +0530

    Add files via upload

---
+++ store/js/main.js
@@ -0,0 +1,20 @@
+$( ".slider-one" )
+    .not( ".slick-intialized" )
+    .slick( {
+    autoplay: true,
+    autoSpeed: 1000,
+        dots: true,
+        prevArrow: ".site-slider .slider-btn .prev",
+        nextArrow: ".site-slider .slider-btn .next"
+});
+
+$( ".slider-two" )
+    .not( ".slick-intialized" )
+    .slick( {
+      
+        prevArrow: ".site-slider-two .prev",
+        nextArrow: ".site-slider-two .next",
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplaySpeed:1000
+    } );
